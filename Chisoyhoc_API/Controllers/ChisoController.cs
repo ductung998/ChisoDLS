404b14737ff1415147deabf07b5102d23407e1cf
Tung <ductung998@gmail.com>
2023-10-30 15:16:03 +0700

    Initiation project

---
+++ Chisoyhoc_API/Controllers/ChisoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chisoyhoc_API.Controllers
+{
+    public class ChisoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/dangnhap/{account}/{password}")]
+        public List<string> dangnhap(string account, string password)
+        {
+            List<string> kq = new List<string> { account, password };
+            return kq;
+        }
+
+        [HttpGet]
+        [Route("api/dangxuat/{account}/{password}")]
+        public List<string> dangxuat(string account, string password)
+        {
+            List<string> kq = new List<string> { account, password };
+            return kq;
+
+        }
+    }
+}
---
+++ Chisoyhoc_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chisoyhoc_API.WebApiApplication" Language="C#" %>
