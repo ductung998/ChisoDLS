3c8742c019e74f8f6cb1d7f3c6b863486d8a7539
ducluong123 <lhduc.lm2001@gmail.com>
2023-10-31 21:06:58 +0700

    Thêm button Test FormIBW

--- Chisoyhoc_Form/GiaodienMau/FormIBW.Designer.cs
+++ Chisoyhoc_Form/GiaodienMau/FormIBW.Designer.cs
@@ -33,6 +33,7 @@
             this.txtGioiTinh = new System.Windows.Forms.TextBox();
             this.txtChieuCao = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -77,17 +78,27 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(226, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Test";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FormIBW
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1028, 609);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtChieuCao);
             this.Controls.Add(this.txtGioiTinh);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormIBW";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -102,6 +113,7 @@
         private System.Windows.Forms.TextBox txtGioiTinh;
         private System.Windows.Forms.TextBox txtChieuCao;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
 
 
     }
